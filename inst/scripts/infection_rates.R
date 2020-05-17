6eb55ec342044b9418f7e331641d5ac09366ff54
lgs85 <lewisspurgin@gmail.com>
2020-05-18 00:54:54 +0100

    changed R

--- inst/scripts/infection_rates.R
+++ inst/scripts/infection_rates.R
@@ -22,7 +22,7 @@ net.sch <- format_network(am2.sch)
 
 scenario_sim2 <- purrr::partial(scenario_sim,
                                 n.sim = nreps, net = net.sch, df = name.class,prop.ascertain = 0,
-                                cap_max_days = 30, R = 6.5, presymrate = 0.4,
+                                cap_max_days = 30, R = 1, presymrate = 0.4,
                                 delay_shape = 1, delay_scale = 1.4, num.initial.cases = 1,
                                 scenario = "nothing",outside = 0.001, sensitivity = "high",
                                 testing = "none", output = "daily", cap_max_tests = NULL)
@@ -44,19 +44,19 @@ res6 <- scenario_sim2(asym.adult = 0.4, asym.child = 0.4,asym.adult.inf = 0.5,
 
 # Bind together results and save output -----------------------------------
 
-res <- bind_rows(res1,res2,res3) %>%
+out <- bind_rows(res1,res2,res3) %>%
   mutate(intervention = rep(c("Children as infectious as adults",
                               "Children 50% as infectious as adults",
                               "Children 10% as infectious as adults"),
                                 each = nrow(res1)))
 
-res2 <- bind_rows(res4,res4,res6) %>%
+out2 <- bind_rows(res4,res5,res6) %>%
   mutate(intervention = rep(c("Children as infectious as adults",
                               "Children 50% as infectious as adults",
                               "Children 10% as infectious as adults"),
                             each = nrow(res4)))
 
 
-saveRDS(res, file = "data-raw/infection_rates_asym90.rds")
-saveRDS(res2, file = "data-raw/infection_rates_asym40.rds")
+saveRDS(out, file = "data-raw/infection_rates_asym90.rds")
+saveRDS(out2, file = "data-raw/infection_rates_asym40.rds")
 
